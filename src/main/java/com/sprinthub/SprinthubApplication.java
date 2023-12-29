c69982cfa80384f8b59b5736c78abfa7fa7cad89
FlashShri <shrikantdhengle3101@gmail.com>
2023-12-29 22:47:59 +0530

    first commit

---
+++ src/main/java/com/sprinthub/SprinthubApplication.java
@@ -0,0 +1,13 @@
+package com.sprinthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinthubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprinthub/SprinthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprinthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
