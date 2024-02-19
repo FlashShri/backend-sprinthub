285c7770c382671e0f98dfa280996feb2b574702
FlashShri <shrikantdhengle3101@gmail.com>
2024-02-20 04:03:57 +0530

    minor changes compliant to frontend

--- src/main/java/com/sprinthub/controller/AdminController.java
+++ src/main/java/com/sprinthub/controller/AdminController.java
@@ -1,14 +1,25 @@
 package com.sprinthub.controller;
 
-import com.sprinthub.entity.Admin;
-import com.sprinthub.exception.adminServiceException;
-import com.sprinthub.service.AdminService;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import com.sprinthub.entity.Admin;
+import com.sprinthub.exception.adminServiceException;
+import com.sprinthub.service.AdminService;
 
 @RestController
 @CrossOrigin
@@ -29,7 +40,8 @@ public class AdminController {
     }
     
     @GetMapping("/getBy/{id}")
-    public ResponseEntity<Admin> getAdminByEmail(@PathVariable Integer id) {
+    @Secured("Admin") 
+    public ResponseEntity<Admin> getAdminById(@PathVariable Integer id) {
         Optional<Admin> admin = adminService.getAdminById(id);
         return admin.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
